f5ea8c286a2595ef841b032ed7f1eb5f4724d383
Prasad <banprasad21@gmail.com>
2022-09-02 08:09:01 +0530

    [Raghav] Add: code to add node to binary tree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using BinarySearchTree;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        BinarySearchTree<int> binarySearchTree = new BinarySearchTree<int>();
+        binarySearchTree.Add(56);
+        binarySearchTree.Add(30);
+        binarySearchTree.Add(70);
+    }
+}
